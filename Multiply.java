ea743318c6e474ac900f13b2d283b035a31835a6
Urvish <ubundela55@gmail.com>
2022-02-25 01:31:47 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    public static void main (String[] args) {
+        int x = 10;
+        int y = 20;
+
+        System.out.println("The product is " + (x*y));
+    }
+    
+}
